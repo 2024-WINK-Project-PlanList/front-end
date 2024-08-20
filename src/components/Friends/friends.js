77960e6d232fefecafa2aa89d94de332b1a5c393
hyeonjin6530 <cola1876@naver.com>
2024-08-20 09:32:09 +0900

    feat: 친구리스트 페이지(친구 유) 구현

---
+++ src/components/Friends/friends.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import { ReactComponent as ProfilPic } from '../../assets/friendsList/profilePic.svg';
+import { ReactComponent as Music } from '../../assets/friendsList/music.svg';
+
+const Friends = ({ profile, name, email, song }) => {
+  return (
+    <div className="flex justify-between items-center h-[100px] mt-[18px] border-b border-[#D8D8D8]">
+      <div className="flex items-center pl-[17px]">
+        {typeof profile === 'string' ? (
+          <img src={profile} alt={name} className="w-[63px] h-[63px]" />
+        ) : (
+          profile
+        )}
+        <div className="pl-[9px]">
+          <div className="font-preSemiBold text-lg">{name}</div>
+          <div className="font-preLight text-xs text-[#3F3F3F]">{email}</div>
+        </div>
+      </div>
+
+      <div className="overflow-hidden flex items-center w-[150px] h-[27px] border border-[#E8E8E8] rounded-lg px-[9px] py-[7px] mr-[7px]">
+        <Music />
+        <div className="font-preRegular text-xs pl-[7px] whitespace-nowrap">
+          {song}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Friends.propTypes = {
+  profile: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  song: PropTypes.string.isRequired,
+};
+export default Friends;
--- src/pages/FriendsList/FriendsList.js
+++ src/pages/FriendsList/FriendsList.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import { ReactComponent as Glass } from '../../assets/friendsList/magnifyingGlass.svg';
+import Friend from '../../components/Friends/friends';
+import { ReactComponent as ProfilePic } from '../../assets/friendsList/profilePic.svg';
 
 const FriendsList = () => {
+  const dummyData = [
+    {
+      profile: ProfilePic,
+      name: '왕연진',
+      email: 'jjini6530@kookmin.ac.kr',
+      song: '졸려요 - 히히',
+    },
+    {
+      profile: ProfilePic,
+      name: '왕연진',
+      email: 'jjini6530@kookmin.ac.kr',
+      song: '졸려요 - 히히',
+    },
+    {
+      profile: ProfilePic,
+      name: '왕연진',
+      email: 'jjini6530@kookmin.ac.kr',
+      song: '졸려요ㅜㅜ- 히히',
+    },
+  ];
+  const isEmpty = dummyData.length === 0;
+
   return (
     <div className="flex flex-col items-center font-preRegular pt-[26px]">
       <div className="relative w-full px-[24px]">
@@ -11,7 +35,25 @@ const FriendsList = () => {
         />
         <Glass className="absolute bottom-[12px] left-[40px] w-[20px] h-[20px] fill-[#3F3F3F]" />
       </div>
-      <div className="text-xl pt-[73px]">새로운 친구를 추가해보세요!</div>
+      <div className="w-full px-[19px]">
+        {isEmpty ? (
+          <div className="flex justify-center text-xl pt-[73px]">
+            새로운 친구를 추가해보세요!
+          </div>
+        ) : (
+          <div>
+            {dummyData.map((item, index) => (
+              <Friend
+                key={index}
+                profile={<item.profile />}
+                name={item.name}
+                email={item.email}
+                song={item.song}
+              />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
