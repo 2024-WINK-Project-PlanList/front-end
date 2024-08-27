f0790978b42296af08894723e97ed3c6c11f2deb
hyeonjin6530 <cola1876@naver.com>
2024-08-27 16:27:54 +0900

    Add: 마이페이지 내 헤더, 푸터 추가

--- src/pages/MyPage/mypage.js
+++ src/pages/MyPage/mypage.js
@@ -5,6 +5,8 @@ import { ReactComponent as SignIcon } from '../../assets/mypage/sign.svg';
 import { ReactComponent as MySentenceIcon } from '../../assets/mypage/mySentence.svg';
 import PlayList from '../../components/PlayList/playList';
 import ModifyProfile from '../../components/Modal/modifyProfile';
+import Header from '../../components/Layout/Header';
+import Footer from '../../components/Layout/Footer';
 
 const MyPage = () => {
   const [profileData, setProfileData] = useState({
@@ -28,62 +30,67 @@ const MyPage = () => {
   }
 
   return (
-    <div className="flex flex-col items-center">
-      {profileData.image ? (
-        <img
-          src={profileData.image}
-          alt="Profile"
-          className="mt-[30px] w-[154px] h-[154px] object-cover rounded-full"
-        />
-      ) : (
-        <ProfilePic className="mt-[30px]" />
-      )}
-      <div className="text-2xl font-preSemiBold pt-[15px]">
-        {profileData.name}
-      </div>
-      <div className="text-base font-preLight text-[#3F3F3F] pt-[7px]">
-        {profileData.email}
-      </div>
-      <div
-        className="flex justify-center items-center text-sm text-white font-preMedium bg-[#6AB6FF] w-[5.8125rem] h-[1.6875rem] rounded-[0.625rem] mt-4"
-        onClick={clickedModifyButton}
-      >
-        내 정보 수정
-      </div>
-      <div className="flex justify-between items-center w-[22rem] h-[3.0625rem] rounded-[0.625rem] border-[0.5px] border-[#D5D5D5] mt-6 pl-[22px] pr-[16px]">
-        <div className="flex items-center">
-          <FriendsIcon />
-          <div className="font-preRegular pl-[6px]">내 친구</div>
+    <>
+      <Header />
+      <div className="flex flex-col items-center">
+        {profileData.image ? (
+          <img
+            src={profileData.image}
+            alt="Profile"
+            className="mt-[30px] w-[154px] h-[154px] object-cover rounded-full"
+          />
+        ) : (
+          <ProfilePic className="mt-[30px]" />
+        )}
+        <div className="text-2xl font-preSemiBold pt-[15px]">
+          {profileData.name}
         </div>
-        <div className="flex items-center">
-          <div className="font-preSemiBold text-xl text-[#5DA4E7] pr-[17px]">
-            650명
+        <div className="text-base font-preLight text-[#3F3F3F] pt-[7px]">
+          {profileData.email}
+        </div>
+        <div
+          className="flex justify-center items-center text-sm text-white font-preMedium bg-[#6AB6FF] w-[5.8125rem] h-[1.6875rem] rounded-[0.625rem] mt-4"
+          onClick={clickedModifyButton}
+        >
+          내 정보 수정
+        </div>
+        <div className="flex justify-between items-center w-[22rem] h-[3.0625rem] rounded-[0.625rem] border-[0.5px] border-[#D5D5D5] mt-6 pl-[22px] pr-[16px]">
+          <div className="flex items-center">
+            <FriendsIcon />
+            <div className="font-preRegular pl-[6px]">내 친구</div>
           </div>
-          <SignIcon />
+          <div className="flex items-center">
+            <div className="font-preSemiBold text-xl text-[#5DA4E7] pr-[17px]">
+              650명
+            </div>
+            <SignIcon />
+          </div>
         </div>
-      </div>
-      <div className="flex justify-between items-center w-[22rem] h-[3.0625rem] rounded-[0.625rem] border-[0.5px] border-[#D5D5D5] mt-6 pl-[22px] pr-[16px]">
-        <div className="flex items-center">
-          <MySentenceIcon />
-          <div className="font-preRegular pl-[6px]">나의 한마디</div>
+        <div className="flex justify-between items-center w-[22rem] h-[3.0625rem] rounded-[0.625rem] border-[0.5px] border-[#D5D5D5] mt-6 pl-[22px] pr-[16px]">
+          <div className="flex items-center">
+            <MySentenceIcon />
+            <div className="font-preRegular pl-[6px]">나의 한마디</div>
+          </div>
+          <div className="font-preLight text-sm max-w-[160px] overflow-hidden text-ellipsis whitespace-nowrap">
+            {profileData.bio}
+          </div>
         </div>
-        <div className="font-preLight text-sm max-w-[160px] overflow-hidden text-ellipsis whitespace-nowrap">
-          {profileData.bio}
+        <div className="w-full pb-[13px] pt-[33px] pl-[33px] font-preSemiBold text-xl">
+          나의 PlayList
         </div>
-      </div>
-      <div className="w-full pb-[13px] pt-[33px] pl-[33px] font-preSemiBold text-xl">
-        나의 PlayList
-      </div>
-      <PlayList music={true} />
+        <PlayList music={true} />
 
-      {modalIsOpen && (
-        <ModifyProfile
-          onClose={closeModal}
-          onSave={handleSaveProfile}
-          profileData={profileData}
-        />
-      )}
-    </div>
+        {!modalIsOpen && <Footer />}
+
+        {modalIsOpen && (
+          <ModifyProfile
+            onClose={closeModal}
+            onSave={handleSaveProfile}
+            profileData={profileData}
+          />
+        )}
+      </div>
+    </>
   );
 };
 export default MyPage;
